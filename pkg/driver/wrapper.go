99833c5e62e487d7d2f14e062c450f9acb79c6c3
Lukas Herman <lherman.cs@gmail.com>
2020-01-01 17:27:28 -0800

    Add video driver state management

--- pkg/driver/wrapper.go
+++ pkg/driver/wrapper.go
@@ -2,6 +2,8 @@ package driver
 
 import uuid "github.com/satori/go.uuid"
 
+import "fmt"
+
 func wrapAdapter(a Adapter) Driver {
 	var d Driver
 	id := uuid.NewV4().String()
@@ -35,6 +37,64 @@ func (w *videoAdapterWrapper) ID() string {
 
 func (w *videoAdapterWrapper) Status() State {
 	return w.state
+}
+
+func (w *videoAdapterWrapper) Open() error {
+	if w.state != StateClosed {
+		return fmt.Errorf("invalid state: driver is already opened")
+	}
+
+	err := w.VideoAdapter.Open()
+	if err == nil {
+		w.state = StateOpened
+	}
+	return err
+}
+
+func (w *videoAdapterWrapper) Close() error {
+	err := w.VideoAdapter.Close()
+	if err == nil {
+		w.state = StateClosed
+	}
+	return err
+}
+
+func (w *videoAdapterWrapper) Start(setting VideoSetting, cb DataCb) error {
+	if w.state == StateClosed {
+		return fmt.Errorf("invalid state: driver hasn't been opened")
+	}
+
+	if w.state == StateStarted {
+		return fmt.Errorf("invalid state: driver has been started")
+	}
+
+	prevState := w.state
+	w.state = StateOpened
+	err := w.VideoAdapter.Start(setting, cb)
+	if err != nil {
+		w.state = prevState
+	}
+	return err
+}
+
+func (w *videoAdapterWrapper) Stop() error {
+	if w.state != StateStarted {
+		return fmt.Errorf("invalid state: driver hasn't been started")
+	}
+
+	err := w.VideoAdapter.Stop()
+	if err == nil {
+		w.state = StateStopped
+	}
+	return err
+}
+
+func (w *videoAdapterWrapper) Settings() []VideoSetting {
+	if w.state == StateClosed {
+		return nil
+	}
+
+	return w.VideoAdapter.Settings()
 }
 
 // TODO: Add state validation
