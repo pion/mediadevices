20f49929d480dcd2e24b8c87177c666014381687
Atsushi Watanabe <atsushi.w@ieee.org>
2020-03-04 19:07:48 +0900

    Add CGO version of decodeYUY2

---
+++ pkg/frame/yuv_nocgo.go
@@ -0,0 +1,43 @@
+// +build !cgo
+
+package frame
+
+import (
+	"fmt"
+	"image"
+)
+
+func decodeYUY2(frame []byte, width, height int) (image.Image, error) {
+	yi := width * height
+	ci := yi / 2
+	fi := yi + 2*ci
+
+	if len(frame) != fi {
+		return nil, fmt.Errorf("frame length (%d) less than expected (%d)", len(frame), fi)
+	}
+
+	y := make([]byte, yi)
+	cb := make([]byte, ci)
+	cr := make([]byte, ci)
+
+	fast := 0
+	slow := 0
+	for i := 0; i < fi; i += 4 {
+		y[fast] = frame[i]
+		cb[slow] = frame[i+1]
+		y[fast+1] = frame[i+2]
+		cr[slow] = frame[i+3]
+		fast += 2
+		slow++
+	}
+
+	return &image.YCbCr{
+		Y:              y,
+		YStride:        width,
+		Cb:             cb,
+		Cr:             cr,
+		CStride:        width / 2,
+		SubsampleRatio: image.YCbCrSubsampleRatio422,
+		Rect:           image.Rect(0, 0, width, height),
+	}, nil
+}
