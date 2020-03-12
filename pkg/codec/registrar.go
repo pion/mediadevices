a510a6e266376ef4ff9d2e2b26423c16be39dcaa
Atsushi Watanabe <atsushi.w@ieee.org>
2020-03-13 01:51:09 +0900

    Fallback codecs
    Add a wrapper function to initialize codec and try next fallback
    codec if one fails.

--- pkg/codec/registrar.go
+++ pkg/codec/registrar.go
@@ -1,6 +1,7 @@
 package codec
 
 import (
+	"errors"
 	"fmt"
 	"io"
 
@@ -14,6 +15,7 @@ var (
 	audioEncoders = make(map[string]AudioEncoderBuilder)
 )
 
+// Register a new EncoderBuilder for the name.
 func Register(name string, builder interface{}) {
 	switch b := builder.(type) {
 	case VideoEncoderBuilder:
@@ -23,6 +25,7 @@ func Register(name string, builder interface{}) {
 	}
 }
 
+// BuildVideoEncoder builds encoder for the codec p.CodecName.
 func BuildVideoEncoder(r video.Reader, p prop.Media) (io.ReadCloser, error) {
 	b, ok := videoEncoders[p.CodecName]
 	if !ok {
@@ -32,6 +35,7 @@ func BuildVideoEncoder(r video.Reader, p prop.Media) (io.ReadCloser, error) {
 	return b(r, p)
 }
 
+// BuildAudioEncoder builds encoder for the codec p.CodecName.
 func BuildAudioEncoder(r audio.Reader, p prop.Media) (io.ReadCloser, error) {
 	b, ok := audioEncoders[p.CodecName]
 	if !ok {
@@ -39,4 +43,76 @@ func BuildAudioEncoder(r audio.Reader, p prop.Media) (io.ReadCloser, error) {
 	}
 
 	return b(r, p)
+}
+
+// NamedVideoCodec stores codec and its name.
+type NamedVideoCodec struct {
+	Name  string
+	Codec func(video.Reader, prop.Media) (io.ReadCloser, error)
+}
+
+// NamedAudioCodec stores codec and its name.
+type NamedAudioCodec struct {
+	Name  string
+	Codec func(audio.Reader, prop.Media) (io.ReadCloser, error)
+}
+
+// VideoEncoderFallbacks packs multiple codecs as a new VideoEncoderBuilder.
+// First codec is tried to initialize first, and the next codec will be tried if failed.
+func VideoEncoderFallbacks(codecs ...NamedVideoCodec) VideoEncoderBuilder {
+	return VideoEncoderBuilder(func(r video.Reader, p prop.Media) (io.ReadCloser, error) {
+		codecParams := map[string]interface{}{}
+		switch cp := p.CodecParams.(type) {
+		case nil:
+		case (map[string]interface{}):
+			codecParams = cp
+		default:
+			return nil, errors.New("CodecParams of VideoEncoderFallbacks must be map[string]interface{}")
+		}
+
+		var rc io.ReadCloser
+		var err error
+		for _, codec := range codecs {
+			if cp, ok := codecParams[codec.Name]; ok {
+				p.CodecParams = cp
+			} else {
+				p.CodecParams = nil
+			}
+			rc, err = codec.Codec(r, p)
+			if err == nil {
+				return rc, nil
+			}
+		}
+		return nil, err
+	})
+}
+
+// AudioEncoderFallbacks packs multiple codecs as a new AudioEncoderBuilder.
+// First codec is tried to initialize first, and the next codec will be tried if failed.
+func AudioEncoderFallbacks(codecs ...NamedAudioCodec) AudioEncoderBuilder {
+	return AudioEncoderBuilder(func(r audio.Reader, p prop.Media) (io.ReadCloser, error) {
+		codecParams := map[string]interface{}{}
+		switch cp := p.CodecParams.(type) {
+		case nil:
+		case (map[string]interface{}):
+			codecParams = cp
+		default:
+			return nil, errors.New("CodecParams of AudioEncoderFallbacks must be map[string]interface{}")
+		}
+
+		var rc io.ReadCloser
+		var err error
+		for _, codec := range codecs {
+			if cp, ok := codecParams[codec.Name]; ok {
+				p.CodecParams = cp
+			} else {
+				p.CodecParams = nil
+			}
+			rc, err = codec.Codec(r, p)
+			if err == nil {
+				return rc, nil
+			}
+		}
+		return nil, err
+	})
 }
