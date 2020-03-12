3876b0c3c5fcbc9114e8a242534ee0121cfaa9c0
Lukas Herman <lherman.cs@gmail.com>
2020-03-12 00:07:19 -0400

    Add preset

--- pkg/codec/x264/params.go
+++ pkg/codec/x264/params.go
@@ -2,4 +2,22 @@ package x264
 
 // Params stores libx264 specific encoding parameters.
 type Params struct {
+	// Faster preset has lower CPU usage but lower quality
+	Preset Preset
 }
+
+// Preset represents a set of default configurations from libx264
+type Preset int
+
+const (
+	PresetUltrafast Preset = iota
+	PresetSuperfast
+	PresetVeryfast
+	PresetFaster
+	PresetFast
+	PresetMedium
+	PresetSlow
+	PresetSlower
+	PresetVeryslow
+	PresetPlacebo
+)
