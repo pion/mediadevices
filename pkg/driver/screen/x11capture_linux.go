5075afc09a0c0002ad172eae0b4a2335ae39fea6
Atsushi Watanabe <atsushi.w@ieee.org>
2020-03-29 11:32:49 +0900

    driver/screen: support X11 16 bit color frame buffer

--- pkg/driver/screen/x11capture_linux.go
+++ pkg/driver/screen/x11capture_linux.go
@@ -7,7 +7,8 @@ package screen
 // #define XUTIL_DEFINE_FUNCTIONS
 // #include <X11/Xutil.h>
 // #include <X11/extensions/XShm.h>
-// void copyRGBA(void *dst, char *src, size_t l) { // 64bit aligned copy
+//
+// void copyBGR24(void *dst, char *src, size_t l) { // 64bit aligned copy
 //   uint64_t *d = (uint64_t*)dst;
 //   uint64_t *s = (uint64_t*)src;
 //   l /= 8;
@@ -21,6 +22,22 @@ package screen
 //     s++;
 //   }
 // }
+//
+// void copyBGR16(void *dst, char *src, size_t l) { // 64bit aligned copy
+//   uint64_t *d = (uint64_t*)dst;
+//   uint32_t *s = (uint32_t*)src;
+//   l /= 8;
+//   for (size_t i = 0; i < l; i ++) {
+//     uint64_t v = *s;
+//     // Reorder BGR to RGB
+//     *d = 0xFF000000FF000000 |
+//          ((v & 0xF8000000) << 8)  | ((v & 0x7E00000) << 21) | ((v & 0x1F0000) << 35) |
+//          ((v & 0xF800) >> 8) | ((v & 0x7E0) << 5) | ((v & 0x1F) << 19);
+//     d++;
+//     s++;
+//   }
+// }
+//
 // char *align64(char *ptr) { // return 64bit aligned pointer
 //   if (((size_t)ptr & 0x07) == 0) {
 //     return ptr;
@@ -35,6 +52,7 @@ import "C"
 
 import (
 	"errors"
+	"fmt"
 	"image"
 	"image/color"
 	"unsafe"
@@ -43,6 +61,15 @@ import (
 const shmaddrInvalid = ^uintptr(0)
 
 type display C.Display
+
+type pixelFormat int
+
+const (
+	pixFmtBGR24 pixelFormat = iota
+	pixFmtRGB24
+	pixFmtBGR16
+	pixFmtRGB16
+)
 
 func openDisplay() (*display, error) {
 	dp := C.XOpenDisplay(nil)
@@ -65,10 +92,11 @@ func (d *display) NumScreen() int {
 }
 
 type shmImage struct {
-	dp  *C.Display
-	img *C.XImage
-	shm C.XShmSegmentInfo
-	b   []byte
+	dp     *C.Display
+	img    *C.XImage
+	shm    C.XShmSegmentInfo
+	b      []byte
+	pixFmt pixelFormat
 }
 
 func (s *shmImage) Free() {
@@ -96,21 +124,47 @@ func (c colorFunc) RGBA() (r, g, b, a uint32) {
 }
 
 func (s *shmImage) At(x, y int) color.Color {
-	addr := (x + y*int(s.img.width)) * 4
-	b := uint32(s.b[addr]) * 0x100
-	g := uint32(s.b[addr+1]) * 0x100
-	r := uint32(s.b[addr+2]) * 0x100
-	return colorFunc(func() (_, _, _, _ uint32) {
-		return r, g, b, 0xFFFF
-	})
+	switch s.pixFmt {
+	case pixFmtBGR24:
+		addr := (x + y*int(s.img.width)) * 4
+		b := uint32(s.b[addr]) * 0x100
+		g := uint32(s.b[addr+1]) * 0x100
+		r := uint32(s.b[addr+2]) * 0x100
+		return colorFunc(func() (_, _, _, _ uint32) {
+			return r, g, b, 0xFFFF
+		})
+	case pixFmtBGR16:
+		addr := (x + y*int(s.img.width)) * 2
+		b1, b2 := s.b[addr], s.b[addr+1]
+		b := uint32(b1>>3) * 0x100
+		g := uint32((b1&0x7)<<3|(b2&0xE0)>>5) * 0x100
+		r := uint32(b2&0x1F) * 0x100
+		return colorFunc(func() (_, _, _, _ uint32) {
+			return r, g, b, 0xFFFF
+		})
+	default:
+		panic("unsupported pixel format")
+	}
 }
 
 func (s *shmImage) RGBAAt(x, y int) color.RGBA {
-	addr := (x + y*int(s.img.width)) * 4
-	b := s.b[addr]
-	g := s.b[addr+1]
-	r := s.b[addr+2]
-	return color.RGBA{R: r, G: g, B: b, A: 0xFF}
+	switch s.pixFmt {
+	case pixFmtBGR24:
+		addr := (x + y*int(s.img.width)) * 4
+		b := s.b[addr]
+		g := s.b[addr+1]
+		r := s.b[addr+2]
+		return color.RGBA{R: r, G: g, B: b, A: 0xFF}
+	case pixFmtBGR16:
+		addr := (x + y*int(s.img.width)) * 2
+		b1, b2 := s.b[addr], s.b[addr+1]
+		b := b1 >> 3
+		g := (b1&0x7)<<3 | (b2&0xE0)>>5
+		r := b2 & 0x1F
+		return color.RGBA{R: r, G: g, B: b, A: 0xFF}
+	default:
+		panic("unsupported pixel format")
+	}
 }
 
 func (s *shmImage) ToRGBA(dst *image.RGBA) *image.RGBA {
@@ -123,8 +177,16 @@ func (s *shmImage) ToRGBA(dst *image.RGBA) *image.RGBA {
 		}
 		dst.Pix = dst.Pix[:l]
 	}
-	C.copyRGBA(unsafe.Pointer(&dst.Pix[0]), s.img.data, C.ulong(len(dst.Pix)))
-	return dst
+	switch s.pixFmt {
+	case pixFmtBGR24:
+		C.copyBGR24(unsafe.Pointer(&dst.Pix[0]), s.img.data, C.ulong(len(dst.Pix)))
+		return dst
+	case pixFmtBGR16:
+		C.copyBGR16(unsafe.Pointer(&dst.Pix[0]), s.img.data, C.ulong(len(dst.Pix)))
+		return dst
+	default:
+		panic("unsupported pixel format")
+	}
 }
 
 func newShmImage(dp *C.Display, screen int) (*shmImage, error) {
@@ -135,6 +197,17 @@ func newShmImage(dp *C.Display, screen int) (*shmImage, error) {
 	depth := int(C.XDefaultDepth(dp, cScreen))
 
 	s := &shmImage{dp: dp}
+
+	switch {
+	case v.red_mask == 0xFF0000 && v.green_mask == 0xFF00 && v.blue_mask == 0xFF:
+		s.pixFmt = pixFmtBGR24
+	case v.red_mask == 0xF800 && v.green_mask == 0x7E0 && v.blue_mask == 0x1F:
+		s.pixFmt = pixFmtBGR16
+	default:
+		fmt.Printf("x11capture: unsupported pixel format (R: %0x, G: %0x, B: %0x)\n",
+			v.red_mask, v.green_mask, v.blue_mask)
+		return nil, errors.New("unsupported pixel format")
+	}
 
 	s.shm.shmid = C.shmget(C.IPC_PRIVATE, C.ulong(w*h*4+8), C.IPC_CREAT|0600)
 	if s.shm.shmid == -1 {
