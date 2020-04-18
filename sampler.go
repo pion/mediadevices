e15b35953868adfb8e0fadc191040b3ea35b5a4a
Lukas Herman <lherman.cs@gmail.com>
2020-04-18 15:07:41 -0400

    Refractor track to be more DRY
    Resolves https://github.com/pion/mediadevices/issues/141
    
     * Remove newVideoTrack and newAudioTrack
     * Add a generic encoderBuilder struct
     * Add newVideoEncoderBuilders and newAudioEncoderBuilders helpers
     * Update sampler to be functional and add support for audio sampler
     * Reduce boilerplates by using closure

--- sampler.go
+++ sampler.go
@@ -1,31 +1,35 @@
 package mediadevices
 
 import (
+	"math"
 	"time"
 
 	"github.com/pion/webrtc/v2/pkg/media"
 )
 
-type sampler struct {
-	track         LocalTrack
-	clockRate     float64
-	lastTimestamp time.Time
+type samplerFunc func(b []byte) error
+
+// newVideoSampler creates a video sampler that uses the actual video frame rate and
+// the codec's clock rate to come up with a duration for each sample.
+func newVideoSampler(t LocalTrack) samplerFunc {
+	clockRate := float64(t.Codec().ClockRate)
+	lastTimestamp := time.Now()
+
+	return samplerFunc(func(b []byte) error {
+		now := time.Now()
+		duration := now.Sub(lastTimestamp).Seconds()
+		samples := uint32(math.Round(clockRate * duration))
+		lastTimestamp = now
+
+		return t.WriteSample(media.Sample{Data: b, Samples: samples})
+	})
 }
 
-func newSampler(track LocalTrack) *sampler {
-	return &sampler{
-		track:         track,
-		clockRate:     float64(track.Codec().ClockRate),
-		lastTimestamp: time.Now(),
-	}
-}
-
-func (s *sampler) sample(b []byte) error {
-	now := time.Now()
-	duration := now.Sub(s.lastTimestamp).Seconds()
-	samples := uint32(s.clockRate * duration)
-	s.lastTimestamp = now
-
-	sample := media.Sample{Data: b, Samples: samples}
-	return s.track.WriteSample(sample)
+// newAudioSampler creates a audio sampler that uses a fixed latency and
+// the codec's clock rate to come up with a duration for each sample.
+func newAudioSampler(t LocalTrack, latency time.Duration) samplerFunc {
+	samples := uint32(math.Round(float64(t.Codec().ClockRate) * latency.Seconds()))
+	return samplerFunc(func(b []byte) error {
+		return t.WriteSample(media.Sample{Data: b, Samples: samples})
+	})
 }
