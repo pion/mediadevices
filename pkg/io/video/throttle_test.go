89fe93cb8a7586663d577494d48d63382d815e4f
Atsushi Watanabe <atsushi.w@ieee.org>
2020-06-03 21:06:17 +0900

    Decrease test data rate for test stability
    Ticker on OSX seems not accurate.

--- pkg/io/video/throttle_test.go
+++ pkg/io/video/throttle_test.go
@@ -9,25 +9,25 @@ import (
 func TestThrottle(t *testing.T) {
 	img := image.NewRGBA(image.Rect(0, 0, 640, 480))
 
-	ticker := time.NewTicker(time.Millisecond)
+	ticker := time.NewTicker(20 * time.Millisecond)
 	defer ticker.Stop()
 
 	var cntPush int
-	trans := Throttle(100)
+	trans := Throttle(50)
 	r := trans(ReaderFunc(func() (image.Image, error) {
 		<-ticker.C
 		cntPush++
 		return img, nil
 	}))
 
-	for i := 0; i < 50; i++ {
+	for i := 0; i < 20; i++ {
 		_, err := r.Read()
 		if err != nil {
 			t.Fatalf("Unexpected error: %v", err)
 		}
 	}
-	cntExpected := 500
-	if cntPush < cntExpected*9/10 || cntExpected*11/10 < cntPush {
+	cntExpected := 20
+	if cntPush < cntExpected*8/10 || cntExpected*12/10 < cntPush {
 		t.Fatalf("Number of pushed images is expected to be %d, but pushed %d", cntExpected, cntPush)
 	}
 	t.Log(cntPush)
