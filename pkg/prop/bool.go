85ab1969945aeb8e8e0ccbf7cc3880c56fcbf777
Lukas Herman <lherman.cs@gmail.com>
2020-06-21 20:39:55 -0400

    Add IsFloat, IsBigEndian, and IsInterleaved props
    * Add bool constraint
    * Add IsFloat, IsBigEndian, and IsInterleaved properties

---
+++ pkg/prop/bool.go
@@ -0,0 +1,30 @@
+package prop
+
+// BoolConstraint is an interface to represent bool value constraint.
+type BoolConstraint interface {
+	Compare(bool) (float64, bool)
+	Value() bool
+}
+
+// BoolExact specifies exact bool value.
+type BoolExact bool
+
+// Compare implements BoolConstraint.
+func (b BoolExact) Compare(o bool) (float64, bool) {
+	if bool(b) == o {
+		return 0.0, true
+	}
+	return 1.0, false
+}
+
+// Value implements BoolConstraint.
+func (b BoolExact) Value() bool { return bool(b) }
+
+// Bool specifies ideal bool value.
+type Bool BoolExact
+
+// Compare implements BoolConstraint.
+func (b Bool) Compare(o bool) (float64, bool) {
+	dist, _ := BoolExact(b).Compare(o)
+	return dist, true
+}
