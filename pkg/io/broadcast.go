d0302f5aed9c658c7a54f5f4f5c37ffe8b8e243f
Lukas Herman <lherman.cs@gmail.com>
2020-09-13 14:24:03 -0700

    Add ring buffer

--- pkg/io/broadcast.go
+++ pkg/io/broadcast.go
@@ -7,8 +7,8 @@ import (
 )
 
 const (
-	maxDataCount = 0xfffffff
-	maskReading  = 0x80000000
+	maskReading         = 1 << 63
+	broadcasterRingSize = 128
 )
 
 var errEmptySource = fmt.Errorf("Source can't be nil")
@@ -19,20 +19,67 @@ type broadcasterData struct {
 	err   error
 }
 
+type broadcasterRing struct {
+	buffer []atomic.Value
+	// reading (1 bit) + reserved (31 bits) + data count (32 bits)
+	state uint64
+}
+
+func newBroadcasterRing() *broadcasterRing {
+	return &broadcasterRing{buffer: make([]atomic.Value, broadcasterRingSize)}
+}
+
+func (ring *broadcasterRing) index(count uint32) int {
+	return int(count) % len(ring.buffer)
+}
+
+func (ring *broadcasterRing) acquire(count uint32) func(*broadcasterData) {
+	// Reader has reached the latest data, should read from the source.
+	// Only allow 1 reader to read from the source. When there are more than 1 readers,
+	// the other readers will need to share the same data that the first reader gets from
+	// the source.
+	state := uint64(count)
+	if atomic.CompareAndSwapUint64(&ring.state, state, state|maskReading) {
+		return func(data *broadcasterData) {
+			i := ring.index(count)
+			ring.buffer[i].Store(data)
+			atomic.StoreUint64(&ring.state, uint64(count+1))
+		}
+	}
+
+	return nil
+}
+
+func (ring *broadcasterRing) get(count uint32) *broadcasterData {
+	for {
+		reading := uint64(count) | maskReading
+		// TODO: since it's lockless, it spends a lot of resources in the scheduling.
+		for atomic.LoadUint64(&ring.state) == reading {
+			// Yield current goroutine to let other goroutines to run instead
+			runtime.Gosched()
+		}
+
+		i := ring.index(count)
+		data := ring.buffer[i].Load().(*broadcasterData)
+		if data.count == count {
+			return data
+		}
+
+		count++
+	}
+}
+
 // Broadcaster is a generic pull-based broadcaster. Broadcaster is unique in a sense that
 // readers can come and go at anytime, and readers don't need to close or notify broadcaster.
 type Broadcaster struct {
 	source atomic.Value
-	// reading (1 bit) + reserved (3 bits) + data count (28 bits)
-	state uint32
-	// TODO: instead of storing only last data. A fixed length ring buffer
-	//       is probably better so that we reduce the amount of data lost.
-	last atomic.Value
+	buffer *broadcasterRing
 }
 
 // NewNewBroadcaster creates a new broadcaster.
 func NewBroadcaster(source Reader) *Broadcaster {
 	var broadcaster Broadcaster
+	broadcaster.buffer = newBroadcasterRing()
 	broadcaster.ReplaceSource(source)
 
 	return &broadcaster
@@ -45,42 +92,25 @@ func NewBroadcaster(source Reader) *Broadcaster {
 // scheduler to decide with goroutines that should run.
 func (broadcaster *Broadcaster) NewReader(copyFn func(interface{}) interface{}) Reader {
 	var currentCount uint32
+	// since ring buffer's state starts with 0, each reader count should start with MAX(uint32),
+	// so that it'll get incremented to zero (with wrap around)
+	currentCount--
 
 	return ReaderFunc(func() (data interface{}, err error) {
-		reading := currentCount | maskReading
-
-		// Reader has reached the latest data, should read from the source.
-		// Only allow 1 reader to read from the source. When there are more than 1 readers,
-		// the other readers will need to share the same data that the first reader gets from
-		// the source.
-		if atomic.CompareAndSwapUint32(&broadcaster.state, currentCount, reading) {
-			nextCount := (currentCount + 1) % maxDataCount
+		currentCount++
+		if push := broadcaster.buffer.acquire(currentCount); push != nil {
 			data, err = broadcaster.source.Load().(Reader).Read()
-			broadcaster.last.Store(&broadcasterData{
+			push(&broadcasterData{
 				data:  data,
 				err:   err,
-				count: nextCount,
+				count: currentCount,
 			})
-
-			atomic.StoreUint32(&broadcaster.state, nextCount)
-
-			currentCount = nextCount
-			data = copyFn(data)
-
-			return
+		} else {
+			ringData := broadcaster.buffer.get(currentCount)
+			data, err, currentCount = ringData.data, ringData.err, ringData.count
 		}
 
-		// Since it's possible for the first reader to finish reading from the source before the current reader
-		// reaches this point, we need to make sure that the global count is still the same and the first reader
-		// is still reading.
-		// TODO: since it's lockless, it spends a lot of resources in the scheduling.
-		for atomic.LoadUint32(&broadcaster.state) == reading {
-			// Yield current goroutine to let other goroutines to run instead
-			runtime.Gosched()
-		}
-
-		last := broadcaster.last.Load().(*broadcasterData)
-		data, err, currentCount = copyFn(last.data), last.err, last.count
+		data = copyFn(data)
 		return
 	})
 }
