a3bd030855b0279f7db1bf3930ac1166f787d3b2
Lukas Herman <lherman.cs@gmail.com>
2020-11-21 14:01:53 -0800

    Remove some hardcoded microphone metadata
    Changes:
      * Use malgo.DeviceInfo to get the microphone metadata
      * Move static buffering to the driver layer

--- track.go
+++ track.go
@@ -379,9 +379,6 @@ func newAudioTrackFromDriver(d driver.Driver, recorder driver.AudioRecorder, con
 		return nil, err
 	}
 
-	// FIXME: The current audio detection and audio encoder can only work with a static latency. Since the latency from the driver
-	//        can fluctuate, we need to stabilize it. Maybe there's a better way for doing this?
-	reader = audio.NewBuffer(int(constraints.selectedMedia.Latency.Seconds() * float64(constraints.selectedMedia.SampleRate)))(reader)
 	return newAudioTrackFromReader(d, reader, selector), nil
 }
 
