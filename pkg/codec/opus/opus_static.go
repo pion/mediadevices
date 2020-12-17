76908aca892979d2a0caa024a4f45df26d5d853f
Lukas Herman <lherman.cs@gmail.com>
2020-12-17 14:53:07 -0800

    Disable stack protection in opus

--- pkg/codec/opus/opus_static.go
+++ pkg/codec/opus/opus_static.go
@@ -8,5 +8,5 @@ package opus
 //#cgo linux,arm64 LDFLAGS: ${SRCDIR}/lib/libopus_linux_arm64.a -lm
 //#cgo linux,amd64 LDFLAGS: ${SRCDIR}/lib/libopus_linux_x64.a -lm
 //#cgo darwin,amd64 LDFLAGS: ${SRCDIR}/lib/libopus_darwin_x64.a
-//#cgo windows,amd64 LDFLAGS: ${SRCDIR}/lib/libopus_windows_x64.a -lssp
+//#cgo windows,amd64 LDFLAGS: ${SRCDIR}/lib/libopus_windows_x64.a
 import "C"
