271a79381298a44e5069f978aa57b2dc99d226cb
Lukas Herman <lherman.cs@gmail.com>
2020-12-17 23:05:08 -0800

    Enhance build system by switching to Makefile
    By switching to Makefile, parallel builds and single build are now
    possible.
    
    Examples:
    
      Parallel Build:
        make -j
    
      Single Build:
        make opus-darwin-x64
    
    Also, since Makefile has a dependency change detection feature, the
    build time is reduced significantly when there are only a few things that
    change, Makefile will not rebuild everything unnecessarily.

--- pkg/codec/opus/opus_static.go
+++ pkg/codec/opus/opus_static.go
@@ -4,9 +4,9 @@ package opus
 
 //#cgo CFLAGS: -I${SRCDIR}/include
 //#cgo CXXFLAGS: -I${SRCDIR}/include
-//#cgo linux,arm LDFLAGS: ${SRCDIR}/lib/libopus_linux_armv7.a -lm
-//#cgo linux,arm64 LDFLAGS: ${SRCDIR}/lib/libopus_linux_arm64.a -lm
-//#cgo linux,amd64 LDFLAGS: ${SRCDIR}/lib/libopus_linux_x64.a -lm
-//#cgo darwin,amd64 LDFLAGS: ${SRCDIR}/lib/libopus_darwin_x64.a
-//#cgo windows,amd64 LDFLAGS: ${SRCDIR}/lib/libopus_windows_x64.a
+//#cgo linux,arm LDFLAGS: ${SRCDIR}/lib/libopus-linux-armv7.a -lm
+//#cgo linux,arm64 LDFLAGS: ${SRCDIR}/lib/libopus-linux-arm64.a -lm
+//#cgo linux,amd64 LDFLAGS: ${SRCDIR}/lib/libopus-linux-x64.a -lm
+//#cgo darwin,amd64 LDFLAGS: ${SRCDIR}/lib/libopus-darwin-x64.a
+//#cgo windows,amd64 LDFLAGS: ${SRCDIR}/lib/libopus-windows-x64.a
 import "C"
