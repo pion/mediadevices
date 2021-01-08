1d1eb5bdcbe918c46719f029682b7fc50d58eeef
Atsushi Watanabe <atsushi.w@ieee.org>
2021-01-08 13:09:05 +0900

    Add test for vpx input image size change

---
+++ pkg/codec/vpx/vpx_test.go
@@ -0,0 +1,89 @@
+package vpx
+
+import (
+	"image"
+	"io"
+	"sync/atomic"
+	"testing"
+
+	"github.com/pion/mediadevices/pkg/codec"
+	"github.com/pion/mediadevices/pkg/frame"
+	"github.com/pion/mediadevices/pkg/io/video"
+	"github.com/pion/mediadevices/pkg/prop"
+)
+
+func TestImageSizeChange(t *testing.T) {
+	for name, factory := range map[string]func() (codec.VideoEncoderBuilder, error){
+		"VP8": func() (codec.VideoEncoderBuilder, error) {
+			p, err := NewVP8Params()
+			return &p, err
+		},
+		"VP9": func() (codec.VideoEncoderBuilder, error) {
+			p, err := NewVP9Params()
+			return &p, err
+		},
+	} {
+		factory := factory
+		t.Run(name, func(t *testing.T) {
+			param, err := factory()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			for name, testCase := range map[string]struct {
+				initialWidth, initialHeight int
+				width, height               int
+			}{
+				"NoChange": {
+					320, 240,
+					320, 240,
+				},
+				"Enlarge": {
+					320, 240,
+					640, 480,
+				},
+				"Shrink": {
+					640, 480,
+					320, 240,
+				},
+			} {
+				testCase := testCase
+				t.Run(name, func(t *testing.T) {
+					var cnt uint32
+					r, err := param.BuildVideoEncoder(
+						video.ReaderFunc(func() (image.Image, func(), error) {
+							i := atomic.AddUint32(&cnt, 1)
+							if i == 1 {
+								return image.NewYCbCr(
+									image.Rect(0, 0, testCase.width, testCase.height),
+									image.YCbCrSubsampleRatio420,
+								), func() {}, nil
+							}
+							return nil, nil, io.EOF
+						}),
+						prop.Media{
+							Video: prop.Video{
+								Width:       testCase.initialWidth,
+								Height:      testCase.initialHeight,
+								FrameRate:   1,
+								FrameFormat: frame.FormatI420,
+							},
+						},
+					)
+					if err != nil {
+						t.Fatal(err)
+					}
+					_, rel, err := r.Read()
+					if err != nil {
+						t.Fatal(err)
+					}
+					rel()
+					_, _, err = r.Read()
+					if err != io.EOF {
+						t.Fatal(err)
+					}
+				})
+			}
+		})
+	}
+}
