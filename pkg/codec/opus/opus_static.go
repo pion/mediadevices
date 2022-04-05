d62a3fefbc03b129d34105e31c9aa0a340bd6240
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2022-04-05 15:18:18 +0200

    Update opus_static.go

--- pkg/codec/opus/opus_static.go
+++ pkg/codec/opus/opus_static.go
@@ -8,5 +8,6 @@ package opus
 //#cgo linux,arm64 LDFLAGS: ${SRCDIR}/lib/libopus-linux-arm64.a -lm
 //#cgo linux,amd64 LDFLAGS: ${SRCDIR}/lib/libopus-linux-x64.a -lm
 //#cgo darwin,amd64 LDFLAGS: ${SRCDIR}/lib/libopus-darwin-x64.a
+//#cgo darwin,arm64 LDFLAGS: ${SRCDIR}/lib/libopus-darwin-arm64.a
 //#cgo windows,amd64 LDFLAGS: ${SRCDIR}/lib/libopus-windows-x64.a
 import "C"
