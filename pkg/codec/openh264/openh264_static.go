e371c0d9555bb5cc913e95c99b4706b906949d21
代码人生 <zjzhang@126.com>
2022-06-02 18:41:39 +0800

    fix:  Fix compilation error on arm64 Mac (#404)

--- pkg/codec/openh264/openh264_static.go
+++ pkg/codec/openh264/openh264_static.go
@@ -8,5 +8,6 @@ package openh264
 //#cgo linux,arm64 LDFLAGS: ${SRCDIR}/lib/libopenh264-linux-arm64.a
 //#cgo linux,amd64 LDFLAGS: ${SRCDIR}/lib/libopenh264-linux-x64.a
 //#cgo darwin,amd64 LDFLAGS: ${SRCDIR}/lib/libopenh264-darwin-x64.a
+//#cgo darwin,arm64 LDFLAGS: ${SRCDIR}/lib/libopenh264-darwin-arm64.a
 //#cgo windows,amd64 LDFLAGS: ${SRCDIR}/lib/libopenh264-windows-x64.a -lssp
 import "C"
