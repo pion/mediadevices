8a498bd959dd855693961e9b1195051f6603791a
Kyle Worcester-Moore <25kylworc@gmail.com>
2022-10-29 20:56:56 -0700

    Add drivers for shell cmd video & audio sources

---
+++ pkg/driver/cmdsource.go/cmdsource.go
@@ -0,0 +1,96 @@
+package cmdsource
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"reflect"
+	"time"
+
+	"github.com/google/shlex"
+	"github.com/pion/mediadevices/pkg/prop"
+)
+
+var (
+	errReadTimeout       = errors.New("read timeout")
+	errInvalidCommand    = errors.New("invalid command")
+	errUnsupportedFormat = errors.New("Unsupported frame format, no frame size function found")
+)
+
+var cmdSourceLabelCounts map[string]uint = make(map[string]uint)
+
+type cmdSource struct {
+	cmdArgs     []string
+	props       []prop.Media
+	readTimeout uint32 // in seconds
+	execCmd     *exec.Cmd
+}
+
+func init() {
+	// No init, call AddVideoCmdSource() or AddAudioCmdSource() to add a command source before getUserMedia().
+}
+
+func newCmdSource(command string, mediaProperties []prop.Media, readTimeout uint32) cmdSource {
+	fmt.Println("New Command Source: " + command)
+	cmdArgs, err := shlex.Split(command) // split command string on whitespace, respecting quotes & comments
+	if err != nil {
+		panic(errInvalidCommand)
+	}
+	return cmdSource{
+		cmdArgs:     cmdArgs,
+		props:       mediaProperties,
+		readTimeout: readTimeout,
+	}
+}
+
+func (c *cmdSource) Open() error {
+	c.execCmd = exec.Command(c.cmdArgs[0], c.cmdArgs[1:]...)
+	return nil
+}
+
+func (c *cmdSource) Close() error {
+	if c.execCmd == nil || c.execCmd.Process == nil {
+		return nil
+	}
+
+	_ = c.execCmd.Process.Signal(os.Interrupt) // send SIGINT to process to stop it
+	done := make(chan error)
+	go func() { done <- c.execCmd.Wait() }()
+	select {
+	case err := <-done:
+		return err // command exited normally or with an error code
+	case <-time.After(10 * time.Second):
+		err := c.execCmd.Process.Kill() // command timed out, kill it & return error
+		return err
+	}
+
+}
+
+func (c *cmdSource) Properties() []prop.Media {
+	return c.props
+}
+
+func (c *cmdSource) addEnvVarsFromStruct(props interface{}) {
+	c.execCmd.Env = os.Environ() // inherit environment variables
+	values := reflect.ValueOf(props)
+	types := values.Type()
+	fmt.Println("Added Environment Variables: ")
+	for i := 0; i < values.NumField(); i++ {
+		name := types.Field(i).Name
+		value := values.Field(i)
+		envVar := fmt.Sprintf("MEDIA_DEVICES_%s=%v", name, value)
+		fmt.Println(envVar + ", ")
+		c.execCmd.Env = append(c.execCmd.Env, envVar)
+	}
+}
+
+func (c *cmdSource) getCmdLabel() string {
+	programName := c.cmdArgs[0]
+	if _, ok := cmdSourceLabelCounts[programName]; ok {
+		cmdSourceLabelCounts[programName]++
+	} else {
+		cmdSourceLabelCounts[programName] = 0
+	}
+	return programName + "_" + fmt.Sprintf("%d", cmdSourceLabelCounts[programName])
+}
---
+++ pkg/driver/cmdsource.go/videocmd.go
@@ -0,0 +1,93 @@
+package cmdsource
+
+import (
+	"image"
+	"io"
+	"os"
+	"time"
+
+	"github.com/pion/mediadevices/pkg/driver"
+	"github.com/pion/mediadevices/pkg/frame"
+	"github.com/pion/mediadevices/pkg/io/video"
+	"github.com/pion/mediadevices/pkg/prop"
+)
+
+type videoCmdSource struct {
+	cmdSource
+}
+
+func AddVideoCmdSource(command string, mediaProperties []prop.Media, readTimeout uint32) error {
+	videoCmdSource := &videoCmdSource{
+		cmdSource: newCmdSource(command, mediaProperties, readTimeout),
+	}
+	if len(videoCmdSource.cmdArgs) == 0 || videoCmdSource.cmdArgs[0] == "" {
+		return errInvalidCommand // no command specified
+	}
+
+	label := videoCmdSource.getCmdLabel()
+	err := driver.GetManager().Register(videoCmdSource, driver.Info{
+		Label:      label,
+		DeviceType: driver.CmdSource,
+		Priority:   driver.PriorityNormal,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *videoCmdSource) VideoRecord(inputProp prop.Media) (video.Reader, error) {
+	getFrameSize, ok := frame.FrameSizeMap[inputProp.FrameFormat]
+	if !ok {
+		return nil, errUnsupportedFormat
+	}
+	frameSize := getFrameSize(inputProp.Width, inputProp.Height)
+
+	decoder, err := frame.NewDecoder(inputProp.FrameFormat)
+	if err != nil {
+		return nil, err
+	}
+
+	// send standard error straight to the console
+	c.execCmd.Stderr = os.Stderr
+
+	// get the command's standard output
+	stdOut, err := c.execCmd.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+	// add environment variables to the command for each media property
+	c.addEnvVarsFromStruct(inputProp.Audio)
+
+	// start the command
+	if err := c.execCmd.Start(); err != nil {
+		return nil, err
+	}
+
+	var buf []byte = make([]byte, frameSize)
+	doneChan := make(chan error)
+	// fmt.Printf("frameSize: %d\n", frameSize)
+	r := video.ReaderFunc(func() (img image.Image, release func(), err error) {
+		go func() {
+			if _, err := io.ReadFull(stdOut, buf); err == io.ErrUnexpectedEOF {
+				doneChan <- io.EOF
+			} else if err != nil {
+				doneChan <- err
+			}
+			doneChan <- nil
+		}()
+
+		select {
+		case err := <-doneChan:
+			if err != nil {
+				return nil, nil, err
+			} else {
+				return decoder.Decode(buf, inputProp.Width, inputProp.Height)
+			}
+		case <-time.After(time.Duration(c.readTimeout) * time.Second):
+			return nil, func() {}, errReadTimeout
+		}
+	})
+
+	return r, nil
+}
