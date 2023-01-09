d5621b27e0e2254d52e7e4ddb2ffd42e4d26c8fe
Clyde Bazile <clyde@viam.com>
2023-01-09 16:53:58 -0500

    nit

--- pkg/driver/camera/camera_windows.go
+++ pkg/driver/camera/camera_windows.go
@@ -12,6 +12,7 @@ import (
 	"sync"
 	"unsafe"
 
+	"github.com/pion/mediadevices/pkg/driver"
 	"github.com/pion/mediadevices/pkg/frame"
 	"github.com/pion/mediadevices/pkg/io/video"
 	"github.com/pion/mediadevices/pkg/prop"
