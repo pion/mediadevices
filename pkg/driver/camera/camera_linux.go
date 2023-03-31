dc8aeea11fcb70bd8d29482976a556a9a75cacb8
martha-johnston <martha.johnston@viam.com>
2023-03-31 15:46:37 -0400

    remove name from linux driver info

--- pkg/driver/camera/camera_linux.go
+++ pkg/driver/camera/camera_linux.go
@@ -116,7 +116,6 @@ func discover(discovered map[string]struct{}, pattern string) {
 			Label:      label + LabelSeparator + reallink,
 			DeviceType: driver.Camera,
 			Priority:   priority,
-			Name:       label,
 		})
 	}
 }
