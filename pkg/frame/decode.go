541463f31a0bad11c142063855f5c9cef3683177
Ryan Boring <ryan.boring@viam.com>
2023-06-02 14:06:26 -0400

    Fix stale links

--- pkg/frame/decode.go
+++ pkg/frame/decode.go
@@ -7,25 +7,26 @@ import (
 type Format string
 
 const (
-	// FormatI420 https://www.fourcc.org/pixel-format/yuv-i420/
+	// FormatI420 https://wiki.videolan.org/YUV#I420
 	FormatI420 Format = "I420"
 	// FormatI444 is a YUV format without sub-sampling
 	FormatI444 Format = "I444"
-	// FormatNV21 https://www.fourcc.org/pixel-format/yuv-nv21/
+	// FormatNV21 https://www.kernel.org/doc/html/v5.9/userspace-api/media/v4l/pixfmt-nv12.html
 	FormatNV21 = "NV21"
-	// FormatNV12 https://www.fourcc.org/pixel-format/yuv-nv12/
+	// FormatNV12 https://www.kernel.org/doc/html/v5.9/userspace-api/media/v4l/pixfmt-nv12.html
 	FormatNV12 = "NV12"
-	// FormatYUY2 https://www.fourcc.org/pixel-format/yuv-yuy2/
+	// FormatYUY2 https://www.kernel.org/doc/html/v5.9/userspace-api/media/v4l/pixfmt-yuyv.html
+	// YUY2 is what Windows calls YUYV
 	FormatYUY2 = "YUY2"
-	// FormatYUYV https://www.fourcc.org/pixel-format/yuv-yuy2/
+	// FormatYUYV https://www.kernel.org/doc/html/v5.9/userspace-api/media/v4l/pixfmt-yuyv.html
 	FormatYUYV = "YUYV"
-	// FormatUYVY https://www.fourcc.org/pixel-format/yuv-uyvy/
+	// FormatUYVY https://www.kernel.org/doc/html/v5.9/userspace-api/media/v4l/pixfmt-uyvy.html
 	FormatUYVY = "UYVY"
 
-	// FormatRGBA https://www.fourcc.org/pixel-format/rgb-rgba/
+	// FormatRGBA https://www.kernel.org/doc/html/v5.9/userspace-api/media/v4l/pixfmt-rgb.html
 	FormatRGBA Format = "RGBA"
 
-	// FormatMJPEG https://www.fourcc.org/mjpg/
+	// FormatMJPEG https://wiki.videolan.org/MJPEG
 	FormatMJPEG = "MJPEG"
 
 	// FormatZ16 https://www.kernel.org/doc/html/v5.9/userspace-api/media/v4l/pixfmt-z16.html
