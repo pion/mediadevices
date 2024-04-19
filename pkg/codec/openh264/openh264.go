f0712d8c9d0b1ff8fbf3d1efc0ab1a5199d49169
Zhenjiang.Zhang <zjzhang@126.com>
2024-04-19 13:27:17 +0800

    Added the set bitrate function for openh264

--- pkg/codec/openh264/openh264.go
+++ pkg/codec/openh264/openh264.go
@@ -95,7 +95,10 @@ func (e *encoder) ForceKeyFrame() error {
 	e.engine.force_key_frame = C.int(1)
 	return nil
 }
-
+func (e *encoder) SetBitRate(bitrate int) error {
+	C.enc_set_bitrate(e.engine, C.int(bitrate))
+	return nil
+}
 func (e *encoder) Controller() codec.EncoderController {
 	return e
 }
