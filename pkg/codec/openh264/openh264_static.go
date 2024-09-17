1711d76f42d213a2807ee29e91431b05df5f6da9
Rodin <brendenaudrina6287@gmail.com>
2024-09-17 23:21:41 +0800

    add llibopenh264-linux-x64.a

--- pkg/codec/openh264/openh264_static.go
+++ pkg/codec/openh264/openh264_static.go
@@ -3,10 +3,12 @@
 
 package openh264
 
+//#cgo LDFLAGS: -static-libstdc++
 //#cgo CFLAGS: -I${SRCDIR}/include
 //#cgo CXXFLAGS: -I${SRCDIR}/include
 //#cgo linux,arm LDFLAGS: ${SRCDIR}/lib/libopenh264-linux-armv7.a
 //#cgo linux,arm64 LDFLAGS: ${SRCDIR}/lib/libopenh264-linux-arm64.a
+//#cgo linux,amd64 LDFLAGS: ${SRCDIR}/lib/libopenh264-linux-x64.a
 //#cgo darwin,amd64 LDFLAGS: ${SRCDIR}/lib/libopenh264-darwin-x64.a
 //#cgo darwin,arm64 LDFLAGS: ${SRCDIR}/lib/libopenh264-darwin-arm64.a
 //#cgo windows,amd64 LDFLAGS: ${SRCDIR}/lib/libopenh264-windows-x64.a -lssp
