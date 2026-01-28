0583d3452abb87d91b9e42699c6483dc6700b428
hexbabe <sean.xiaochen.yu@gmail.com>
2026-01-28 17:25:57 -0500

    Add nomicrophone build tag for optional microphone support
    - Changed from platform-specific (!windows) to opt-out build tag (!nomicrophone)
    - Microphone support included by default (non-breaking)
    - Use -tags nomicrophone to exclude when malgo deps unavailable
    - Useful for cross-compilation and minimal builds
    - Renamed microphone_windows.go to microphone_stub.go for clarity

--- pkg/driver/microphone/microphone.go
+++ pkg/driver/microphone/microphone.go
@@ -1,5 +1,5 @@
-//go:build !windows
-// +build !windows
+//go:build !nomicrophone
+// +build !nomicrophone
 
 package microphone
 
--- pkg/driver/microphone/microphone_windows.go
+++
@@ -1,7 +0,0 @@
-//go:build windows
-// +build windows
-
-package microphone
-
-// Windows stub - microphone support not available on Windows
-// This file exists to allow the package to compile on Windows without malgo dependencies
